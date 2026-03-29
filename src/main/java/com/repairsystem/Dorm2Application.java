932b9f4350abc5540bfa8e59eb19d0d6c62061ba
Drom <2878445295@qq.com>
2026-03-29 14:41:17 +0800

    111

--- src/main/java/com/repairsystem/Dorm2Application.java
+++ src/main/java/com/repairsystem/Dorm2Application.java
@@ -1,13 +1,14 @@
 package com.repairsystem;
-
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class Drom2Application {
+@MapperScan("com.repairsystem.mapper")
+public class Dorm2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Drom2Application.class, args);
+        SpringApplication.run(Dorm2Application.class, args);
     }
-
 }
